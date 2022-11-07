8b04e582a859309e1283948acc4f4e7dc8729436
Yuki Kikuchi <yuki-kikuchi@mail.nissan.co.jp>
2022-11-07 14:45:27 +0900

    (part2) 5. Add interactivity を実装

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    //なぜconst?
+    return const MaterialApp(
       title: 'Startup Name Generator',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Startup Name Generator'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -32,53 +26,99 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  /*
-  *  Set型
-  * (ドキュメント引用)Set is preferred to List because a properly implemented Set doesn't allow duplicate entries.
-  * →重複した値を持たないListみたいな感じ
-  */
+
   final _saved = <WordPair>{};     // add
   final _biggerFont = TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return const Divider();
-
-        final index = i ~/ 2;
-
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        //suggestions[index]があるか、ないか(bool)
-        final alreadySaved = _saved.contains(_suggestions[index]); // add
-        return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
+    return Scaffold(   // NEW from here ...
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        //actionsで右側にボタンを追加できる
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            //おすと_pushSavedを呼び出す
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
           ),
+        ],
+      ),
+      body:  ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
 
-          //iconを表示（右端にくるのはなぜ？）
-          trailing: Icon(
-            alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved ? Colors.red : null,
-            //
-            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-          ),
-          onTap: () {          // NEW from here ...
-            setState(() {
-              //リストをタップした時の動作
-              //_saved内に_suggestions[indes]がない場合(alreadysaved == true)にadd, addされていた場合(alreadysaved == true)はremove
-              if (alreadySaved) {
-                _saved.remove(_suggestions[index]);
-              } else {
-                _saved.add(_suggestions[index]);
-              }
-            });                // to here.
-          },
-        );
-      },
+          final index = i ~/ 2;
+
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          //suggestions[index]があるか、ないか(bool)
+          final alreadySaved = _saved.contains(_suggestions[index]); // add
+          return ListTile(
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerFont,
+            ),
+
+            //iconを表示（右端にくるのはなぜ？）
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+              //
+              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+            ),
+            onTap: () {          // NEW from here ...
+              setState(() {
+                //リストをタップした時の動作
+                //_saved内に_suggestions[indes]がない場合(alreadysaved == true)にadd, addされていた場合(alreadysaved == true)はremove
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });                // to here.
+            },
+          );
+        },
+      ),
+    );
+  }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      // Add lines from here...
+      //MaterialPageRouteを引数として指定することで遷移時にMaterialDesignに則ったアニメーションをしてくれる
+      MaterialPageRoute<void>(
+        builder: (context) {
+          //_saved内の項目をmap型に変換→ListTitle型に変換してreturn
+          final tiles = _saved.map(
+                (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+
+          //項目ごとを線で分割
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ), // ...to here.
     );
   }
 }
